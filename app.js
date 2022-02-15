f642217a7afdef63993b25b2cacac078a68409fa
Vipul Tanwar <vipultanwar45@gmail.com>
2022-02-15 18:52:32 +0530

    Schema Design

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/Rest-API",{ useNewUrlParser: true, useUnifiedTopology: true})
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const categorySchema = new mongoose.Schema({
+    categoryName : String
+})
+
+const productSchema = new mongoose.Schema({
+    productName : String,		
+    qtyPerUnit : Number,		
+    unitPrice : Number,			
+    unitInStock : Number,		
+    discontinued : Boolean,	
+    categoryId : [{ 
+        type: mongoose.Schema.Types.ObjectId , 
+        ref: 'category'}]
+
+
+})
+
+const Category = mongoose.model("category", categorySchema)
+const Product = mongoose.model("product", productSchema)
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
